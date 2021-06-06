694b40e8d7cfb2e76642ca3525ffe4f42d686582
Kyoko <kyokoconnect@gmail.com>
2021-06-06 13:41:39 -0400

    lab done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,39 +1,46 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ sortBy, onChangeSort, filterBy, onChangeFilter }) {
+  function handleSortChange(event) {
+    onChangeSort(event.target.value);
+  }
+
+  function handleFilterChange(event) {
+    onChangeFilter(event.target.value);
+  }
   return (
     <div>
-      <strong>Sort by:</strong>
-      <label>
-        <input
-          type="radio"
-          value="Alphabetically"
-          name="sort"
-          checked={null}
-          onChange={null}
-        />
-        Alphabetically
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="Price"
-          name="sort"
-          checked={null}
-          onChange={null}
-        />
-        Price
-      </label>
-      <br />
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
-    </div>
+    <strong>Sort by:</strong>
+    <label>
+      <input
+        type="radio"
+        value="Alphabetically"
+        name="sort"
+        checked={sortBy === "Alphabetically"}
+        onChange={handleSortChange}
+      />
+      Alphabetically
+    </label>
+    <label>
+      <input
+        type="radio"
+        value="Price"
+        name="sort"
+        checked={sortBy === "Price"}
+        onChange={handleSortChange}
+      />
+      Price
+    </label>
+    <br />
+    <label>
+      <strong>Filter:</strong>
+      <select onChange={handleFilterChange} value={filterBy}>
+        <option value="Tech">Tech</option>
+        <option value="Sportswear">Sportswear</option>
+        <option value="Finance">Finance</option>
+      </select>
+    </label>
+  </div>
   );
 }
 
